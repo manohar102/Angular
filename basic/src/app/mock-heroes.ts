fabf3ec4a25ed2bbf922363fba2be71df25975b9
manohar krishna <manoharkrishna102@gmail.com>
2022-04-03 17:31:02 +0530

    Added new repo "basic"

---
+++ basic/src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import {Hero} from './Hero';
+
+export const HEROES: Hero[] = [
+    {id: 1, name: "Manohar Krishna"},
+    {id: 2, name: "Bhargav"},
+    {id: 3, name: "Kashyap"},
+    {id: 4, name: "Deepak"},
+    {id: 5, name: "Shanmukh"}
+];
